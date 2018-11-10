3447e5547628950f3844e5f5d9409e84ada23b4e
Nicole <nlin.nicole@gmmail.com>
2018-11-10 15:28:58 -0500

    Pig eats rock, fixed animal switchting

--- Assets/Scripts/animalController.cs
+++ Assets/Scripts/animalController.cs
@@ -3,22 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class animalController : MonoBehaviour {
-    public float speed;
-    public float jump;
+    public float pigJump = 5;
+    public float bunnyJump = 15;
+    public float mouseJump = 0;
+
+    public float pigSpeed = 15;
+    public float bunnySpeed = 20;
+    public float mouseSpeed = 25;
+
+    private float speed;
+    private float jumpForce;
+    private Vector3 jump;
+
+    private bool isGounded;
 
     private int animalType = 0;
+    private bool isChanged = false;
     private bool isMoving = true;
+
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 3.0f, 0.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Move();
+        if (Input.GetKeyDown(KeyCode.Space) && isGounded)
+        {
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGounded = false;
+        } else
+        {
+            Move();
+        }
         checkAnimalType();
 	}
+
+    private void OnCollisionStay()
+    {
+        isGounded = true;
+    }
 
     void Move()
     {
@@ -40,54 +67,68 @@ public class animalController : MonoBehaviour {
             position.x++;
             this.transform.position = position;
         }
+    }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Floor")
         {
-            GetComponent<Rigidbody>().velocity = Vector3.up * jump;
+            isMoving = false;
+            if (!isChanged)
+            {
+                switchAnimalType();
+                checkAnimalType();
+            }
+
+        }
+
+        if (col.gameObject.tag == "Rock")
+        {
+            if (this.tag == "Piggy")
+            {
+                Destroy(col.gameObject);
+                Debug.Log("ate rock");
+            }
         }
     }
 
+    void switchAnimalType()
+    {
+        isChanged = true;
+        if (animalType == 0)
+        {
+            animalType = 1;
+            Debug.Log("changed to bunny");
+        }
+        else if (animalType == 1)
+        {
+            animalType = 2;
+            Debug.Log("changed to mouse");
+        }
+        else if (animalType == 2)
+        {
+            animalType = 0;
+            Debug.Log("changed to piggy");
+        }
+        checkAnimalType();
+    }
 
     void checkAnimalType()
     {
         if (animalType == 0)
         {
-            jump = 5;
-            speed = 10;
+            jumpForce = pigJump;
+            speed = pigSpeed;
         }
         else if (animalType == 1)
         {
-            jump = 15;
-            speed = 17;
+            jumpForce = bunnyJump;
+            speed = bunnySpeed;
         }
         else
         {
-            jump = 0;
-            speed = 25;
-        }
-    }
-
-    void OnCollisionEnter(Collision col)
-    {
-
-        if(col.gameObject.tag == "Floor")
-        {
-            isMoving = false;
-            if (animalType == 0)
-            {
-                animalType = 1;
-                Debug.Log("changed to bunny");
-            }
-            else if (animalType == 1){
-                animalType = 2;
-                Debug.Log("changed to mouse");
-            }
-            else if (animalType == 2)
-            {
-                animalType = 0;
-                Debug.Log("changed to piggy");
-            }
-            checkAnimalType();
+            jumpForce = mouseJump;
+            speed = mouseSpeed;
         }
     }
 }
