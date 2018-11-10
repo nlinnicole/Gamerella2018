d1968da33e5e57d89a6c212e519910c6d8dd73c3
Nicole <nlin.nicole@gmmail.com>
2018-11-10 13:25:05 -0500

    Added basic movement behaviour

---
+++ Assets/Scripts/cameraConroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraConroller : MonoBehaviour {
+
+    public GameObject player;
+    float offset;
+
+    // Use this for initialization
+    void Start () {
+        offset = transform.position.z - player.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z + offset);
+	}
+}
